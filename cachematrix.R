dd6c3717581d2f87a85ad9c4707e11965d1e49fc
skiale <pas.pelle@gmail.com>
2018-03-10 22:00:36 -0800

    hey

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
 
-## Write a short comment describing this function
+## Make a function that (1) deletes any matrix inverse stored in the cache
+## (2) defines the getter for the vector x and (3) defines the setter and the getter 
+##for the inverse 'g' and (4) prints a list of all the f(x) to the parent environment
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+					g <- NULL
+					
+					  set <- function(y){
+					    x <<- y
+					    g <<- NULL
+					  }
+					  
+				  get <- function() x
+				  setinverse <- function(inverse) g <<- inverse
+				  getinverse <- function() g
+				  
+				  list( set = set, get = get,
+				        setinverse = setinverse,
+				        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Make the inverse of the matrix returned by makeCacheMatrix.
+## if the inverse 'g' is null it will compute the matrix inverse of the input object 
+## if the 'g' is not null it will be returned to the parent environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          g <- z$getinverse()
+          
+		 if(!is.null(g)){
+		    message("getting cached data")
+		    return(g)
+		    }
+		    
+	    data <- z$get()
+	    g <- solve(data, ...)
+	    z$setinverse(g)
+	    g
 }
